743b1dc7a3aa903845c16a24e02a201d94ba406a
Yoav Damari <105441254+Yoav221@users.noreply.github.com>
2022-11-02 15:51:06 +0200

    fixing the function

--- create_samples.py
+++ create_samples.py
@@ -5,10 +5,9 @@ import pandas as pd
 
 
 def create_if_not_exist(input_vid=INPUT_VID, sample_video_path=SAMPLE_VIDEO_PATH, sample_frame_path=SAMPLE_FRAME_PATH, sample_name=SAMPLE_NAME):
+
     if not os.path.exists(sample_video_path) or (os.stat(sample_video_path).st_size == 0):
         print("Creating Sample Videos ----->")
-        ()
-
         frames_data, width_data, height_data = create_samples(main_video_path=input_vid, explosion_name='single_explosion',
                                                               non_explosion_name='non_explosion', sample_name=sample_name,
                                                               frame_jump=FRAME_JUMP, width_jump=WIDTH_JUMP, height_jump=HEIGHT_JUMP, dim_limit=DIM_LIMIT)
@@ -26,8 +25,10 @@ def create_if_not_exist(input_vid=INPUT_VID, sample_video_path=SAMPLE_VIDEO_PATH
     else:
         print('Sample frames already exist!')
 
+    return frames_data, width_data, height_data
 
-x, y, z = create_if_not_exist()
+
+frames_data, width_data, height_data = create_if_not_exist()
 index_df = pd.DataFrame(
-    data={'frame_index': x, 'width_index': y, 'hegith_index': z})
+    data={'frames_data': frames_data, 'width_data': width_data, 'height_data': height_data})
 index_df.to_csv(SAMPLE_INDEX_DF, index=False)
