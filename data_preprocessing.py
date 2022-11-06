615433ef77fada280203480c255623ef53d20738
Yoav Damari <105441254+Yoav221@users.noreply.github.com>
2022-11-06 12:46:52 +0200

    crating FrameData

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,7 +1,9 @@
 from functions import *
 
-# Make the videos ready for training - creating frame seq out of the videos
+
 if not os.path.exists(TRAIN_DATA) or (os.stat(TRAIN_DATA).st_size == 0):
     os.mkdir(TRAIN_DATA)
-    save_frames_from_multiple_videos(CLASS_1_VIDEO_DATA, TRAIN_DATA, CLASS_1_NAME)
-    save_frames_from_multiple_videos(CLASS_2_VIDEO_DATA, TRAIN_DATA, CLASS_2_NAME)
+    class1 = FrameData(CLASS_1_VIDEO_DATA, TRAIN_DATA, CLASS_1_NAME)
+    class2 = FrameData(CLASS_2_VIDEO_DATA, TRAIN_DATA, CLASS_2_NAME)
+    class1.save_frames_from_multiple_videos()
+    class2.save_frames_from_multiple_videos()
